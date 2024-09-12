96a7b986e5ae1294afd00a01fb9dd039ae0d9031
Chavez0296 <luis.chavez1996@gmail.com>
2024-09-12 13:30:47 -0700

    comments

--- calculator.py
+++ calculator.py
@@ -1,20 +1,20 @@
-def add(x, y, z):
-    return x + y + z
+def add(x, y, z):    #declare add function that accepts 3 arguments
+    return x + y + z # returns the addition of 3 arguments passed in 
 
-def subtract(x, y, z):
-    return z - y - x
+def subtract(x, y, z):# declare subtract function that accepts 3 arguments
+    return z - y - x  # returns the subtraction of the 3 arguments in reverse order
 
-def multiply(x, y):
-     return x * y
+def multiply(x, y): # declare multiply function that takes two arguments
+     return x * y   # returns the multiplication of the two arguments
 
-def divide(x, y):
-    if y == 0:
-        print(f"{x}/{y}: Having a denominator of 0 will reult in a ZeroDivisionError!")
-        return
-    return x / y
+def divide(x, y):   # delcare divide function that takes two arguments
+    if y == 0:      # if y == 0 condition to notify user that dividing by 0 is not possible
+        print(f"{x}/{y}: Having a denominator of 0 will reult in a ZeroDivisionError!") #print error notice
+        return          # returns None if this happens
+    return x / y        # else it returns the division of x / y
 
-expression = divide(multiply(add(1,1,1),1),multiply(subtract(1,4,6),1))
-print(expression)
+expression = divide(multiply(add(1,1,1),1),multiply(subtract(1,4,6),1)) # initialize expression to hold math expression  ((x+y+z)*x)/(z-y-x)*y
+print(expression)   #prints the result of the calculation
 
-undefined = divide(4,0)
-print(undefined)
+undefined = divide(4,0)  #example to show the division by 0 error 
+print(undefined)  # prints the example
--- zooMngSys.py
+++ zooMngSys.py
@@ -1,46 +1,48 @@
-class Animal:
-    def __init__(self, name, species, legs, habitat) -> None:
+class Animal: #creating Animal class
+    def __init__(self, name, species, legs, habitat) -> None: #constructor to construct an instance of an Animal with specific data
         self.name = name
         self.species = species
         self.legs = legs
         self.habitat = habitat
-        self.animDict = {
+        self.animDict = {    #initialize dictionary to hold all data pertaining to intance of Animal
             "name": "",
             "species": "",
             "legs": "",
             "habitat": "",
         }
-        self.animDict["name"] = self.name
+        self.animDict["name"] = self.name#putting all the data associated with intance of Animal into a dictionary
         self.animDict["species"] = self.species
         self.animDict["legs"] = self.legs
         self.animDict["habitat"] = self.habitat
-    def print_animal_details(self, animDict):
-        print(f"Name: {animDict['name']}, Species: {animDict['species']}, Legs: {animDict['legs']}, Habitat: {animDict['habitat']}")
-    def classify(self, legs):
-        newLegs = int(legs)
-        if newLegs == 0:
-            return "Snake or Fish"
-        elif newLegs == 2:
-            return "Bird or Human"
-        elif newLegs == 4:
-            return "Mammal or Reptile"
+
+    def print_animal_details(self, animDict): #print animal details function that passes in dictionary of animal instance
+        print(f"Name: {animDict['name']}, Species: {animDict['species']}, Legs: {animDict['legs']}, Habitat: {animDict['habitat']}") #prints data according to assignment requirements
+
+    def classify(self, legs): #classify function declaration
+        newLegs = int(legs)   #convert legs into an int and store it in place holder
+        if newLegs == 0:      # if legs is 0 
+            return "Snake or Fish" #this will return 
+        elif newLegs == 2:    #if legs is 2 then 
+            return "Bird or Human" #this returns
+        elif newLegs == 4:    #if legs is 4 then
+            return "Mammal or Reptile" #this returns 
         else:
-            return "Insect or Spider"
+            return "Insect or Spider" #legs is greater than 4 then this returns.
 
 
-animalList = []
-animalList.append(Animal("Lion", "Mammal", 4, "Savannah"))
+animalList = []  #initialize list to store Animal instances
+animalList.append(Animal("Lion", "Mammal", 4, "Savannah")) #add all the animals to the list
 animalList.append(Animal("Cow", "Mammal", 4, "Grassland"))
 animalList.append(Animal("Shark","Fish", 0, "Ocean"))
 animalList.append(Animal("Spider", "Insect", 6, "Backyard"))
 animalList.append(Animal("Human", "Mammal", 2, "Everywhere"))
 animalList.append(Animal("Dog", "Mammal", 4, "Land"))
 
-for i in range(len(animalList)):
+for i in range(len(animalList)): #iterate through the list and call the print_animal_details
     animalList[i].print_animal_details(animalList[i].animDict)
 
 print("=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=")
 
-for i in range(len(animalList)):
-    print(f"{animalList[i].animDict['name']}: {animalList[i].classify(animalList[i].animDict['legs'])}")
+for i in range(len(animalList)): #iterate through the list and print what the classification of each animal in the list is based on legs
+    print(f"{animalList[i].animDict['name']}: {animalList[i].classify(animalList[i].animDict['legs'])}") #used f string to print based on assignment requirement to print animal name along side classification
 
